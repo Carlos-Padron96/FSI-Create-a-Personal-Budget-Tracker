8bd371bdbbff01ece023789097d8f2b03dd3699e
Carlos-Padron96 <cpadron96@yahoo.com>
2022-01-24 19:00:36 -0800

    first part done

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
+for(let i= 0; i < weeklyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+weeklyExpenses = weeklyExpenses + answer
+console.log(answer)
+}
 // Your Code Here
